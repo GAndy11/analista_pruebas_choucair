374134c3c3d305cb2ec9616467baa9a0748f3e1f
Alexander Echeverry Torres <alexanderecheverry11@gmail.com>
2021-01-16 14:09:16 -0500

    Creación de la primera tarea

--- src/test/java/co/com/choucair/certification/proyectobase/stepdefinitions/ChoucairAcademyStepDefinitions.java
+++ src/test/java/co/com/choucair/certification/proyectobase/stepdefinitions/ChoucairAcademyStepDefinitions.java
@@ -1,6 +1,7 @@
 package co.com.choucair.certification.proyectobase.stepdefinitions;
 
-import cucumber.api.java.en.Before;
+import co.com.choucair.certification.proyectobase.tasks.OpenUp;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -10,20 +11,20 @@ import net.serenitybdd.screenplay.actors.OnlineCast;
 public class ChoucairAcademyStepDefinitions {
 
     @Before
-    public void setStage(){ onStage.setTheStage(new OnlineCast()); }
+    public void setStage(){ OnStage.setTheStage(new OnlineCast()); }
 
     @Given("^than Alexander wants to learn automation at the academy Choucair$")
     public void thanAlexanderWantsToLearnAutomationAtTheAcademyChoucair() {
+        OnStage.theActorCalled("Alexander").wasAbleTo(OpenUp.thePage());
 
     }
 
-    @When("^he search for the course Recursos Atomatización Bancolombia on the Choucair Academy platform$")
-    public void heSearchForTheCourseRecursosAtomatizaciónBancolombiaOnTheChoucairAcademyPlatform() {
-
+    @When("^he search for the course Recursos Atomatizacion Bancolombia on the Choucair Academy platform$")
+    public void heSearchForTheCourseRecursosAtomatizacionBancolombiaOnTheChoucairAcademyPlatform() {
     }
 
-    @Then("^he finds the course called resources Recursos Automatización Bancolombia$")
-    public void heFindsTheCourseCalledResourcesRecursosAutomatizaciónBancolombia() {
 
+    @Then("^he finds the course called resources Recursos Automatizacion Bancolombia$")
+    public void heFindsTheCourseCalledResourcesRecursosAutomatizacionBancolombia() {
     }
 }
